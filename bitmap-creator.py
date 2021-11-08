ad979deafe00a8fce27edf8dfee01149176c7169
Miguel <jemiguelrivas@gmail.com>
2021-11-08 12:54:04 -0500

    adding functionality to buttons

--- bitmap-creator.py
+++ bitmap-creator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import tkinter as tk
 
-current_color = 0
+current_color = 15
 grid_size = 16
 buttons = [[0 for x in range(grid_size)] for y in range(grid_size)]
 colors = [
@@ -25,24 +25,39 @@ colors = [
 
 win = tk.Tk()
 win.title("Bitmap Creator - " + str(current_color))
+win.resizable(False, False)
 
 def change_color_btn(x,y):
-  win.title("Bitmap Creator - " + str(x) + str(y))
-  # buttons[x][y].config(text=current_color)
+  buttons[x][y].config(
+    text=str(current_color),
+    bg=colors[current_color]
+  )
 
 def change_color(color):
-  current_color = color
+  global current_color
   win.title("Bitmap Creator - " + str(color))
+  current_color = int(color)
 
 for x in range(grid_size):
   for y in range(grid_size):
-    buttons[x][y] = tk.Button(text=str(current_color), width=2, height=2, bg=colors[current_color], command=change_color_btn(x,y))
+    buttons[x][y] = tk.Button(
+      text=str(0),
+      width=2,
+      height=2,
+      bg=colors[0],
+      command=lambda x=x, y=y: change_color_btn(x,y)
+    )
     buttons[x][y].grid(column=x, row=y)
 
 color_btn = [0 for x in range(grid_size)]
 for index in range(grid_size):
-  color_btn[index] = tk.Button(text=index, width=2, height=2, bg=colors[index], command=change_color(index))
+  color_btn[index] = tk.Button(
+    text=index,
+    width=2,
+    height=2,
+    bg=colors[index],
+    command=lambda index=index: change_color(index)
+  )
   color_btn[index].grid(column=index, row=grid_size)
-
 
 win.mainloop()

3c05e0d25950346abcabd674fcc4bd49819b4d87
Miguel <jemiguelrivas@gmail.com>
2021-11-08 14:22:53 -0500

    adding menu bar

--- bitmap-creator.py
+++ bitmap-creator.py
@@ -22,10 +22,44 @@ colors = [
   "#fffe01",
   "#fefff1",
 ]
+colors_name = [
+  "Black",
+  "Blue",
+  "Green",
+  "Cyan",
+  "Red",
+  "Magenta",
+  "Brown",
+  "Light Grey",
+  "Dark Grey",
+  "Light Blue",
+  "Light Green",
+  "Light Cyan",
+  "Light Red",
+  "Light Magenta",
+  "Yellow",
+  "White",
+]
 
-win = tk.Tk()
-win.title("Bitmap Creator - " + str(current_color))
-win.resizable(False, False)
+def clear_values():
+  global current_color
+  current_color = 15
+  win.title("Bitmap Creator - " + colors_name[current_color] + "(" + str(current_color) + ")")
+  for x in range(grid_size):
+    for y in range(grid_size):
+      buttons[x][y].config(
+        text=str(0),
+        bg=colors[0]
+      )
+
+def close_window():
+  win.destroy()
+
+def save_pascal_file():
+  win.title("Save")
+
+def save_cpp_file():
+  win.title("Save")
 
 def change_color_btn(x,y):
   buttons[x][y].config(
@@ -35,8 +69,44 @@ def change_color_btn(x,y):
 
 def change_color(color):
   global current_color
-  win.title("Bitmap Creator - " + str(color))
+  win.title("Bitmap Creator - " + colors_name[color] + "(" + str(color) + ")")
   current_color = int(color)
+
+# defining window
+win = tk.Tk()
+win.title("Bitmap Creator - " + colors_name[current_color] + "(" + str(current_color) + ")")
+win.resizable(False, False)
+
+# creating menu bar
+menu1 = tk.Menu()
+win.config(menu=menu1)
+
+# creating options for the menu bar
+file_menu = tk.Menu(menu1, tearoff=0)
+save_menu = tk.Menu(menu1, tearoff=0)
+edit_menu = tk.Menu(menu1, tearoff=0)
+colors_menu = tk.Menu(menu1, tearoff=0)
+help_menu = tk.Menu(menu1, tearoff=0)
+
+# adding options to the menu bar
+menu1.add_cascade(label="File", menu=file_menu)
+menu1.add_cascade(label="Options", menu=edit_menu)
+menu1.add_cascade(label="Help", menu=help_menu)
+
+file_menu.add_command(label="New File", command=clear_values)
+file_menu.add_cascade(label="Save As", menu=save_menu)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=close_window)
+
+edit_menu.add_cascade(label="Set Color", menu=colors_menu)
+
+help_menu.add_command(label="About", command=close_window)
+
+save_menu.add_command(label="C++", command=save_cpp_file)
+save_menu.add_command(label="Pascal", command=save_pascal_file)
+
+for index in range(len(colors)):
+  colors_menu.add_command(label=colors_name[index], command=lambda index=index: change_color(index))
 
 for x in range(grid_size):
   for y in range(grid_size):
